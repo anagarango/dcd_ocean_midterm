88006af9881b8896e57399d216dae21674d4a01d
AnaArango <aarango4@my.bcit.ca>
2022-03-04 12:54:43 -0800

    Fixing Layout and Components to my section

--- comps/text.js
+++ comps/text.js
@@ -5,8 +5,9 @@ var template_text = document.createElement("template"); //<template> </template>
 template_text.innerHTML = `
 <style>
 p{
+    position: relative;
     width: 250px;
-    font-size: 16px;
+    font-size: 17px;
     color: white;
     text-align: center;
     font-weight: normal;
@@ -30,18 +31,26 @@ class TheText extends HTMLElement {
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
     connectedCallback(){
         this.shadowRoot.appendChild(template_text.content.cloneNode(true)); //use the template to make a clone
-        
+        document.querySelector("the-text").style.position="absolute";
+        this.shadowRoot.querySelector("p").style.top = this.getAttribute("top");
+        this.shadowRoot.querySelector("p").style.left = this.getAttribute("left");
         this.shadowRoot.querySelector("p").innerText = this.getAttribute("content");
         this.shadowRoot.querySelector("p").style.color = this.getAttribute("color");
         this.shadowRoot.querySelector("p").style.width = this.getAttribute("width");
         this.shadowRoot.querySelector("p").style.fontWeight = this.getAttribute("font-weight");
         this.shadowRoot.querySelector("p").style.fontSize = this.getAttribute("font-size");
         this.shadowRoot.querySelector("p").style.textAlign = this.getAttribute("alignment");
-        this.shadowRoot.querySelector("p").style.position = this.getAttribute("position");
-        this.shadowRoot.querySelector("p").style.marginTop = this.getAttribute("top");
-        this.shadowRoot.querySelector("p").style.marginLeft = this.getAttribute("left");
+        document.querySelector("body").onscroll = (e) => console.log(e.target.body.scrollTop);
+        this.HandleContScroll(e.target.scrollTop);
     }
-
+    HandleContScroll(scrollNum=0){
+        console.log("scroll", scrollNum);
+        if(scrollNum === 0){
+           document.querySelector(".ana_section").style.cssText = `
+                display: none
+           `
+        }
+    }
     //To-do - CREATE THE FUNCTIONALITIES HERE!
 }
 
